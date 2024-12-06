941a79a0b79051401d0a74ea394bd6118463d9af
而已 <13423228+tzyhxm123@user.noreply.gitee.com>
2024-12-06 15:01:46 +0800

    deploy

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div>
+        <h1>Hello World</h1>
+        <p>Hello World</p>
+    </div>
+);
+
